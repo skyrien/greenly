32bdfc3f3f069debcb967ba6149da20b0f1b2d34
Alexander Joo <skyrien@gmail.com>
2014-03-27 16:24:51 -0700

    First update in a while

--- Greenly/src/com/skyrien/greenly/RetailActivity.java
+++ Greenly/src/com/skyrien/greenly/RetailActivity.java
@@ -4,28 +4,34 @@
  * It hosts the store information data, and supports the list and map views
  * as fragments.
  * 
+ * A 
+ * 
  */
 
 
 package com.skyrien.greenly;
 
+import android.database.Cursor;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.FragmentManager;
+import android.support.v4.app.LoaderManager;
+import android.support.v4.content.CursorLoader;
+import android.support.v4.content.Loader;
+import android.support.v4.widget.SimpleCursorAdapter;
 import android.util.Log;
 
 public class RetailActivity extends FragmentActivity {
-	
 	public static final String TAG = "RetailActivity";
+	
+
 	
 	// these are mappings for the string intent that is passed
 	public static final String LOADFRAGMENT = "com.skyrien.greenly.loadfragment";
 	public static final int LIST = 1;
 	public static final int MAP = 2;
 	public static final int BOOKMARKS = 3;
-	
-	private int requestedFragment;
 	
 	// The overridden onCreate method is for the activity. It will create
 	// the fragment manager, as well as set up the fragment to be loaded.
@@ -38,16 +44,25 @@ public class RetailActivity extends FragmentActivity {
 		setContentView(R.layout.activity_retail);
 		Log.d(TAG, "OnCreate() called");
 		
+		/* Let's begin the process of loading our data here. The RetailDb Class
+		 * represents the "Model" of the DB schema, and will be accessed via a
+		 * Loader. We are using the Loader
+		 *  		
+		*/
+		
 		// This is the fragment manager for Greenly RetailActivity
 		FragmentManager fm = getSupportFragmentManager();
 		Fragment fragment = fm.findFragmentById(R.id.fragmentContainer);
 		
 		// Find the requested fragment in the intent
 		// Params include the String key, and a default int return value
-		requestedFragment = getIntent().getIntExtra(LOADFRAGMENT, 0);
+		int requestedFragment = getIntent().getIntExtra(LOADFRAGMENT, 0);
 		Log.d(TAG, "Request for " + requestedFragment + " found");
 		
 		// Load the fragment as requested
+
+		// NOTE THAT WE MAY NEED TO SEND A REFERENCE TO THE RETAILDB
+		// INSTANTIATED HERE TO THE FRAGMENT!!!  
 		switch(requestedFragment) {
 			case 0: {
 				Log.e(TAG, "RetailActivity loaded with no requested fragment.");
@@ -86,7 +101,18 @@ public class RetailActivity extends FragmentActivity {
 			}
 
 		}
-	
+
 	}
+
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 	
 }
--- Greenly/src/com/skyrien/greenly/RetailListFragment.java
+++ Greenly/src/com/skyrien/greenly/RetailListFragment.java
@@ -1,15 +1,27 @@
 package com.skyrien.greenly;
 
+import android.database.Cursor;
 import android.os.Bundle;
 import android.support.v4.app.ListFragment;
+import android.support.v4.app.LoaderManager;
+import android.support.v4.content.CursorLoader;
+import android.support.v4.content.Loader;
+import android.support.v4.widget.SimpleCursorAdapter;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 public class RetailListFragment extends ListFragment {
+					//implements	LoaderManager.LoaderCallbacks<Cursor> 
 
 	public static final String TAG = "ListFragment";
+	
+	// This is where we will declare some loader variables.
+	//private static final int LOADER_ID = 0;
+	
+	// This adapter binds data to listview
+	//private SimpleCursorAdapter mCursorAdapter;
 	
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
@@ -19,6 +31,9 @@ public class RetailListFragment extends ListFragment {
 		// Use this to update the app title bar
 		//getActivity().setTitle(R.string.app_name);
 		
+		//getActivity().getSupportLoaderManager().initLoader(LOADER_ID, null, this);
+		//mCursorAdapter = new SimpleCursorAdapter(
+				//getActivity().getApplicationContext(), R.layout.
 	}
 	
 	public View onCreateView(LayoutInflater inflater, ViewGroup parent,
@@ -30,5 +45,28 @@ public class RetailListFragment extends ListFragment {
 		
 		return v;
 	}
-
+	
+	/* THIS SECTION IS IF WE'RE USING CURSORLOADERS.
+	@Override
+	public Loader<Cursor> onCreateLoader(int id, Bundle args) {
+		
+		// TODO
+		return new CursorLoader(getActivity().getApplicationContext());
+	}
+	
+	@Override
+	public void onLoadFinished(Loader<Cursor> loader, Cursor cursor) {
+		switch (loader.getId()) {
+			case LOADER_ID:
+				mCursorAdapter.swapCursor(cursor);
+				break;
+		}
+	}
+	
+	@Override
+	public void onLoaderReset(Loader<Cursor> loader) {
+		mCursorAdapter.swapCursor(null);
+	}
+	 */
+	
 }
